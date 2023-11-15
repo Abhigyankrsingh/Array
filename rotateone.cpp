46b51971ae7705908f00c95dffb3b259e4ab8767
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-15 19:40:34 +0530

    rotate by 1 place

---
+++ rotateone.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+void leftrot(int arr[], int n){
+    int temp = arr[0];
+
+    for(int i =1; i<n;i++){
+        arr[i-1] = arr[i];
+
+        
+    }
+    arr[n-1] = temp;
+}
+
+
+int main(){
+
+  int arr[4] = {1,2,3,4} , n =4;
+
+  leftrot(arr,n);
+
+  for(int i =0; i<n;i++){
+    cout << arr[i];
+  }
+
+}
