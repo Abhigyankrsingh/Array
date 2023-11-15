cbbe7220c8208273e9ad997cfa158c908234d9a9
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-15 13:04:09 +0530

    Code_Update

---
+++ removeDup.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+
+int remv(int arr[], int n){
+
+    int temp[n];
+
+    temp[0] = arr[0];
+     int res = 1;
+    for(int i =0; i<n;i++){
+      
+      if(temp[res-1] != arr[i]){
+         temp[res] = arr[i];
+         res++;
+      }
+    }
+
+    return res;
+}
+
+int main(){
+
+
+int arr[4] = {1,33,33,33} , n=4;
+
+
+n= remv(arr, n);
+
+for(int i =0; i<n;i++){
+    cout << arr[i] ;
+}
+
+
+}
