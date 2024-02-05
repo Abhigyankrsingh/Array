1f37243d0bf640ee1edc832bb320a8eef488fdb5
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-05 21:30:35 +0530

    #

--- binary.cpp
+++ binary.cpp
@@ -1,68 +1,42 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
+int binarySearch(int arr[], int size, int key) {
+    int start = 0;
+    int end = size - 1;
 
-int binarySearch(int arr [], int size, int key){
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
 
- int start = 0;
+        if (arr[mid] == key) {
+            cout << "Key is found at index " << mid << endl;
+            return mid;
+        }
 
- int end =  size -1;
-
- 
-
-
- if ( start <=  end){
-
-    int mid = start + (end - start/2);
-
-    if( arr[mid] == key){
-        cout << "Key is found" << arr[mid] << endl;
-        return mid;
-    
+        if (key > arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
     }
 
-
-    if(key > arr[mid]){
-        start = mid + 1;
-    }
-
-     else {
-        end = mid - 1;
-     }
-
-     mid = start + (end - start/2);
-
-
- }
-
-  return -1;
-
-
+    cout << "Key not found" << endl;
+    return -1;
 }
 
-int main(){
+int main() {
+    int n;
+    cin >> n;
 
-  int n;
+    int k;
+    cin >> k;
 
-  cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
+    cout << "Index is at " << binarySearch(arr, n, k) << endl;
 
-  int k ;
-
-  cin >>k;
-
-  int arr[n] ;
-
-  for(int i =0; i <n; i++){
-     cin >>arr[i];
-  }
-
-
-   cout << "Index is at" << binarySearch(arr, n, k);
-
-
-
-
-
+    return 0;
 }
