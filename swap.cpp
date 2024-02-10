af916ce827fa47956090cd7dbe3b1b77c0a3d17b
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-10 21:58:37 +0530

    Code_Updated

--- swap.cpp
+++ swap.cpp
@@ -1,38 +1,64 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+
+//   void swapnU(int &a, int &b){
+//     int temp = a;
+//     a = b;
+//     b = temp; 
+//   }
+
+//   void printa (int arr[], int n){
+
+//     for(int i =0; i<n; i++){
+//         cout<< arr[i] << " ";
+//     }
+//   }
+
+
+//   void swapat(int arr[], int size){
+    
+
+//     for(int i =0; i <size; i+=2){
+//         swapnU(arr[i], arr[i +1]);
+//     }
+//   }
+
+
+
+// int main(){
+
+
+//     int even[8] = {2,3,31,3,4,1,6,5};
+
+//     swapat( even, 8);
+
+//     printa(even, 8);
+// }
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
 
-  void swapnU(int &a, int &b){
-    int temp = a;
-    a = b;
-    b = temp; 
-  }
+void swapU(int &a, int &b){
 
-  void printa (int arr[], int n){
+  int temp = a;
 
-    for(int i =0; i<n; i++){
-        cout<< arr[i] << " ";
-    }
-  }
+   a = b;
+
+   b = temp;
+}
 
 
-  void swapat(int arr[], int size){
-    
-
-    for(int i =0; i <size; i+=2){
-        swapnU(arr[i], arr[i +1]);
-    }
-  }
 
 
 
 int main(){
 
 
-    int even[8] = {2,3,31,3,4,1,6,5};
 
-    swapat( even, 8);
 
-    printa(even, 8);
 }
