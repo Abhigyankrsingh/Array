efc4a4b49ad6d214340fe0d67499d3f02b31d963
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-17 21:49:19 +0530

    Code_Updated

--- amstrong.cpp
+++ amstrong.cpp
@@ -2,39 +2,32 @@
 
 using namespace std;
 
-
 bool amstro(int num, int digit){
 
   int n = num;  
-  int ans =0;
+  int ans = 0;
   int rem;
 
   while(n){
-    rem = n%10;
-
-    n = n/10;
-
+    rem = n % 10;
+    n = n / 10;
     ans = ans + pow(rem, digit);
   }
 
   if(ans == num){
-    return 1;
+    return true;
   } else {
-    return 0;
+    return false;
   }
-    
 }
 
-
-
-int count (int n){
+int digitCount(int n){
 
     int cnt = 0;
 
     while (n){
         cnt++;
         n /= 10;
-        
     }
 
     return cnt;
@@ -44,11 +37,12 @@ int main(){
 
     int n;
 
+    cout << "Enter a number: ";
     cin >> n;
 
-    int digit = count(n);
+    int digit = digitCount(n);
 
-    cout << amstro(n,digit);
+    cout << amstro(n, digit);
 
-
+    return 0;
 }
