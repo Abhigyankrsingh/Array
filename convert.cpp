b62486ce0cd72cd0c3fc5cf4555e3e631ea3c292
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-17 18:39:55 +0530

    Code_Updated

---
+++ convert.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void convert(char name){
+
+    char c = name   - 'a'  + 'A' ;
+
+    cout << c << endl;
+}
+
+
+
+int main(){
+
+    char c;
+
+    cin >> c;
+
+    convert(c);
+}
