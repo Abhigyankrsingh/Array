cd2c928d5291337af9d3ba3ae1ffe7ec50506016
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-23 20:36:16 +0530

    Code_Updated

--- removemin.cpp
+++ removemin.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+
+
+
+int largest (int arr[], int n){
+
+    int ans = INT16_MAX;
+
+    for(int i =0; i <n;i++){
+        if (arr[i] < ans){
+            ans = arr[i];
+        }
+
+    }
+
+    return ans;
+}
+
 int main(){
 
     int n;
@@ -12,5 +29,7 @@ int main(){
         cin >> arr[i];
     }
 
-    
+   cout << largest(arr,n);
+
+
 }
