db6e22efa4dc6b5a36e8a74cb220e51ca82526ec
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-27 16:59:04 +0530

    Code_Updated

---
+++ fibo.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Enter the number" << endl;
+
+
+    cin >>n;
+
+    vector<int> arr(n);
+
+    arr[0] = 1;
+
+    arr[1] = 1;
+
+    for( int i = 2; i < n; i++){
+
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+   
+   cout << arr[n-1];
+   return 0;
+}
